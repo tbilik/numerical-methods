226194efb49f2ca485512908b3ea1021b1f4cb09
Timothy Bilik <timothy@bilikfamily.com>
2022-07-26 21:21:58 -0400

    finished bisection method

--- ch6/ch6test.cpp
+++ ch6/ch6test.cpp
@@ -5,11 +5,32 @@
 TEST_CASE("bisectionMethod")
 {
     int i;
-    double c = -5;
-    double d = 0;
-    i = bisectionMethod(c, d, 0.0001, 1);
+    double a = 0;
+    double b = 2;
+    // function A, positive roots
+    i = bisectionMethod(a, b, 0.0001, 0);
     cout << "Function A: Iterations: " << i
-         << " A: " << c
-         << " B: " << d;
+         << " A: " << a
+         << " B: " << b << endl;
+    a = 2;
+    b = 5;
+    i = bisectionMethod(a, b, 0.0001, 0);
+    cout << "Function A: Iterations: " << i
+         << " A: " << a
+         << " B: " << b << endl;
+    // function B, negative roots
+    a = -5;
+    b = 0;
+    i = bisectionMethod(a, b, 0.0001, 1);
+    cout << "Function B: Iterations: " << i
+         << " A: " << a
+         << " B: " << b << endl;
+    // function C, first positive root
+    a = 1;
+    b = 2;
+    i = bisectionMethod(a, b, 0.0001, 2);
+    cout << "Function C: Iterations: " << i
+         << " A: " << a
+         << " B: " << b << endl;
     REQUIRE(true);
 }
